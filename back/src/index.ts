4717d6e9cb9b018c0ad18900b7256e5f1b474ec4
Edartruwu <edartru.contactos@gmail.com>
2025-06-05 00:10:27 -0500

    add scripts to backend

--- back/src/index.ts
+++ back/src/index.ts
@@ -6,6 +6,16 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+app.get('/health', (c) => {
+  const now = new Date()
+  const limaTime = new Intl.DateTimeFormat('en-US', {
+    hour: '2-digit',
+    hour12: false,
+    timeZone: 'America/Lima',
+  }).format(now)
+  return c.json({ hour: limaTime, status: 'Todo bien uwu' })
+})
+
 export default {
   port: 8080,
   fetch: app.fetch,
